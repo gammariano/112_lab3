45f989f643dd44e44b50c2204a0f84e30060e890
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-02-15 18:22:29 +0700

    Prime Time

--- 112_lab3_2.c
+++ 112_lab3_2.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int a,p,n,i,j;
+    int a,i,j;
     scanf("%d" ,&n);
+    int p[n],a[n];
     for(i=0;i<n;i++)
     {
-        scanf("%d %d" ,&p,&a);
+        scanf("%d %d" ,&p[i],&a[i]);
     }
 }
