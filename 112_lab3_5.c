ac2f3a414892beba53cd78f266c9262fdfb578cf
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-02-15 19:36:09 +0700

    Lucky Number

--- 112_lab3_5.c
+++ 112_lab3_5.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 int main()
 {
-    int num,i=2,x,v=0;
+    int num,i=2,x,c=0;
     scanf("%d" ,&num);
     x=num;
     while(x!=1)
     {
-
+        while(x%i==0)
+        {
+            x/=i;
+            if(x%i==0)
+            {
+                c++;
+                break;
+            }
+        }
     }
 
 }
