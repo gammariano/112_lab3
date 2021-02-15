cd4665ab8901cf0ba17004342c3db88992ad8f0f
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-02-15 17:19:35 +0700

    Factorization

--- 112_lab3_3.c
+++ 112_lab3_3.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,num;
-    scanf("%d " ,&num);
+    int i=2,j,num;
+    scanf("%d" ,&num);
+    while(num!=1)
+    {
+        if(num%i==0)
+        {
+
+        }
+    }
 }
